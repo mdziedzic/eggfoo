de184a3391007e10fd90a8e37277d33c85439cc1
Michael Dziedzic <mdz@eggfoo.com>
2015-02-11 09:59:04 -0800

    added animated header on top

--- js/main.js
+++ js/main.js
@@ -3,6 +3,23 @@ $(document).ready(function () {
     $('.bg-holder').parallaxScroll({
         friction: 0.6
     });
+    
+    
+    
+    
+    var waypoint = new Waypoint({
+        element: $("#home h1"),
+        handler: function (direction) {
+//            $.notify(direction)
+            if (direction === "down") {
+                $("header").addClass("header-show");
+                $("header").removeClass("header-hide");
+            } else if (direction === "up") {
+                $("header").addClass("header-hide");
+                $("header").removeClass("header-show");
+            }
+        }
+    })        
 
 });
 
