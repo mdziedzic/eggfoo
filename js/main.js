fba579d0a6cdc388671cf4f929d33c3b73d07ad2
Michael Dziedzic <mdz@eggfoo.com>
2015-02-11 18:59:15 -0800

    added nav-menu
    can jump around sections of site via nav-menu, with scrolling between sections.

--- js/main.js
+++ js/main.js
@@ -5,12 +5,11 @@ $(document).ready(function () {
     });
     
     
-    
+    // -------------------------------------------------------  waypoints    
     
     var waypoint = new Waypoint({
         element: $("#home h1"),
         handler: function (direction) {
-//            $.notify(direction)
             if (direction === "down") {
                 $("header").addClass("header-show");
                 $("header").removeClass("header-hide");
@@ -19,12 +18,47 @@ $(document).ready(function () {
                 $("header").removeClass("header-show");
             }
         }
-    })        
+    });       
+    
+    var wp_section = new Waypoint({
+        element: $("#services"),
+        handler: function(direction) {
+            $.notify(direction);
+        }
+    });
 
     
-    $("#nav-nobar, #nav-bar").mouseover(function() {
-        alert("bob");
-    })
+    
+    // -------------------------------------------------------  navigation
+    
+    // navigaton show/hide
+    $("#nav-menu").hide();
+    $("#nav-nobar, #nav-bar, #nav-menu").hover(
+            function() {
+                $("#nav-menu").stop().fadeToggle();
+            }, function() { 
+                $("#nav-menu").stop().fadeToggle();
+            }
+    );
+
+    //  navigation highlight hovered-over link
+    $("#nav-menu p").hover(
+        function() {
+            $(this).addClass("nav-menu-select");
+        }, function() {
+            $(this).removeClass("nav-menu-select");
+        }
+    );
+    
+    // navigation scroll to page sections
+    $("a[href^='#']").click(function(event) {
+        var target = $($(this).attr("href"));
+        event.preventDefault();
+        $("html, body").animate({
+            scrollTop: target.offset().top
+        }, 500);
+    });    
+    
     
     
 });
