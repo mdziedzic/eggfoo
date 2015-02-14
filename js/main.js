760fd58a57bf1c2a69d88dd945ece151f8ea90be
Michael Dziedzic <mdz@eggfoo.com>
2015-02-13 17:43:48 -0800

    javascript/jquery optimization

--- js/main.js
+++ js/main.js
@@ -1,87 +1,97 @@
-$(document).ready(function () {
+/*global $, jQuery, alert, Waypoint */
 
-    $('.bg-holder').parallaxScroll({
-        friction: 0.6
-    });
+$(document).ready(function () {
     
+    "use strict";
+
+    // -------------------------------------------------------  waypoints
     
-    // -------------------------------------------------------  waypoints    
-    
-    var waypoint = new Waypoint({
-        element: $("#home h1"),
-        handler: function (direction) {
-            if (direction === "down") {
-                $("header").addClass("header-show");
-                $("header").removeClass("header-hide");
-            } else if (direction === "up") {
-                $("header").addClass("header-hide");
-                $("header").removeClass("header-show");
-            }
-        }
-    });       
-        
     function setupWaypoint(sPrev, sNext, navMenu, navMenuHighlight) {
-        var sNextMenu = navMenu + sNext;
-        var sPrevMenu = navMenu + sPrev;
-        var wp = new Waypoint({
-            element: $("#" + sNext),
-            handler: function(direction) {
-                if (direction === "down") {
-                    $(sNextMenu).addClass(navMenuHighlight);
-                    $(sPrevMenu).removeClass(navMenuHighlight);
-                } else if (direction === "up") {
-                    $(sNextMenu).removeClass(navMenuHighlight);
-                    $(sPrevMenu).addClass(navMenuHighlight);
-                }
-            },
-            offset: "50%"
-        });
+        var sNextMenu = navMenu + sNext,
+            sPrevMenu = navMenu + sPrev,
+            wp = new Waypoint({
+                element: $("#" + sNext),
+                handler: function (direction) {
+                    if (direction === "down") {
+                        $(sNextMenu).addClass(navMenuHighlight);
+                        $(sPrevMenu).removeClass(navMenuHighlight);
+                    } else if (direction === "up") {
+                        $(sNextMenu).removeClass(navMenuHighlight);
+                        $(sPrevMenu).addClass(navMenuHighlight);
+                    }
+                },
+                offset: "50%"
+            });
         return wp;
     }
 
-    var navMenu = ".nav-menu-";
-    var navMenuHighlight = "nav-menu-location";
-    
-    $(".nav-menu-home").addClass(navMenuHighlight);
-    
-    var wp_services = setupWaypoint("home", "services", navMenu, navMenuHighlight);
-    var wp_work = setupWaypoint("services", "work", navMenu, navMenuHighlight);
-    var wp_about = setupWaypoint("work", "about", navMenu, navMenuHighlight);
-    var wp_contact = setupWaypoint("about", "contact", navMenu, navMenuHighlight);
+    var navMenu = "#nav-menu-",
+        navMenuHighlight = "nav-menu-location",
+        
+        wp_services = setupWaypoint("home", "services", navMenu, navMenuHighlight),
+        wp_work = setupWaypoint("services", "work", navMenu, navMenuHighlight),
+        wp_about = setupWaypoint("work", "about", navMenu, navMenuHighlight),
+        wp_contact = setupWaypoint("about", "contact", navMenu, navMenuHighlight),
+        
+        wp_header = new Waypoint({
+            element: $("#home").find("h1"),
+            handler: function (direction) {
+                if (direction === "down") {
+                    $("header").addClass("header-show");
+                    $("header").removeClass("header-hide");
+                } else if (direction === "up") {
+                    $("header").addClass("header-hide");
+                    $("header").removeClass("header-show");
+                }
+            }
+        });
      
+    $("#nav-menu-home").addClass(navMenuHighlight); // for when page first loads
   
+    
     
     // -------------------------------------------------------  navigation
     
     // navigaton show/hide
     $("#nav-menu").hide();
     $("#nav-nobar, #nav-bar, #nav-menu").hover(
-            function() {
-                $("#nav-menu").stop().fadeToggle();
-            }, function() { 
-                $("#nav-menu").stop().fadeToggle();
-            }
+        function () {
+            $("#nav-menu").stop().fadeToggle();
+        },
+        function () {
+            $("#nav-menu").stop().fadeToggle();
+        }
     );
 
     //  navigation highlight hovered-over link
-    $("#nav-menu p").hover(
-        function() {
+    $("#nav-menu").find("p").hover(
+        function () {
             $(this).addClass("nav-menu-select");
-        }, function() {
+        },
+        function () {
             $(this).removeClass("nav-menu-select");
         }
     );
     
     // navigation scroll to page sections
-    $("a[href^='#']").click(function(event) {
-        var target = $($(this).attr("href"));
+    $("a[href^='#']", "#nav-menu").click(function (event) {
+        var $target = $($(this).attr("href"));
         event.preventDefault();
         $("html, body").animate({
-            scrollTop: target.offset().top
+            scrollTop: $target.offset().top
         }, 500);
-    });    
+    });
     
     
     
+    // -------------------------------------------------------  parallaxScroll
+    
+    $('.bg-holder').parallaxScroll({
+        friction: 0.6
+    });
+    
+    
+    
+
 });
 
