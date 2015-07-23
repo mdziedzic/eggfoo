bab0b15cfa28b29df61cce6bc70b60730e044ee8
Michael Dziedzic <mdz@eggfoo.com>
2015-07-22 18:07:50 -0700

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+ /*global require */
+
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    uglifycss = require('gulp-uglifycss'),
+    concat = require('gulp-concat'),
+    imagemin = require('gulp-imagemin'),
+    htmlreplace = require('gulp-html-replace'),
+    del = require('del');
+
+// delete the build directory
+gulp.task('clean', function() {
+    return del('build/');
+});
+
+gulp.task('js', ['clean'], function () {
+    gulp.src(['js/**/*.js', '!js/vendor/modernizr-2.8.3-custom.min.js', 
+              '!js/vendor/jquery-2.1.4.min.js', '!js/egg-mover.js'])
+        .pipe(uglify())
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('build/js/'));
+    gulp.src(['js/vendor/jquery-2.1.4.min.js', 'js/vendor/modernizr-2.8.3-custom.min.js',
+             'js/egg-mover.js'])
+        .pipe(gulp.dest('build/js/'));
+});
+
+gulp.task('css', ['clean'], function () {
+    gulp.src('css/main.css')
+        .pipe(uglifycss())
+        .pipe(gulp.dest('build/css/'));
+});
+
+gulp.task('image', ['clean'], function () {
+    gulp.src(['img/*', 'img/*/*'])
+        .pipe(imagemin())
+        .pipe(gulp.dest('build/img/'));
+});
+
+gulp.task('root', ['clean'], function () {
+    gulp.src('work/**/*', { "base" : 'work'})
+        .pipe(gulp.dest('build/work/'));
+    return gulp.src(['*.html', '*.php', '*.txt', '*.png', '*.xml', '*.ico', '*.json', '.htaccess'])
+        .pipe(gulp.dest('build/'));
+});
+
+gulp.task('htmladjust', ['root'], function () {
+    gulp.src('build/header.html')
+        .pipe(htmlreplace({
+            'jsheader': 'js/modernizr-2.8.3-custom.min.js'
+        }))
+        .pipe(gulp.dest('build/'));
+    gulp.src('build/footer.html')
+        .pipe(htmlreplace({
+            'jsfooter': 'js/app.js'
+        }))
+        .pipe(gulp.dest('build/'));
+});
+
+
+gulp.task('default', ['root', 'js', 'css', 'image', 'htmladjust']);
+    
